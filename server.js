54f23fa75bbb066c2a010cf19c22551833e4aa31
Sean <seanghunkim1006@gmail.com>
2024-11-30 14:18:05 -0500

    Add Node.js configuration files

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// Serve static files
 app.use(express.static(path.join(__dirname)));
 
-// Handle all routes by serving index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
